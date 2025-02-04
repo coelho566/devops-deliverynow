afc093dcaa41c98245b7203cef177ed2726eb9af
coelho566 <leandrofreestyle18@gmail.com>
2025-02-04 17:38:14 -0300

    Config infra file zip

--- main.tf
+++ main.tf
@@ -190,3 +190,276 @@ module "gateway" {
   depends_on = [module.nlb]
 }
 
+# Keycloak
+module "rds_keycloak" {
+  source                          = "terraform-aws-modules/rds/aws"
+  identifier                      = "demo_aws_account_name-mariadb-keycloak-rds"
+  allow_major_version_upgrade     = "true"
+  engine                          = "mariadb"
+  instance_class                  = "db.t3.medium"
+  allocated_storage               = 40
+  max_allocated_storage           = 100
+  port                            = "3306"
+  username                        = "keycloak"
+  create_random_password          = false
+  password                        = var.keycloak_password
+  subnet_ids                      = [module.global_vpc.database_subnets[0], module.global_vpc.database_subnets[1]]
+  vpc_security_group_ids          = [module.rds-keycloak-sg.security_group_id]
+  maintenance_window              = "Sat:04:00-Sat:06:00"
+  backup_window                   = "02:00-06:00"
+  backup_retention_period         = 1
+  copy_tags_to_snapshot           = true
+  storage_encrypted               = true
+  apply_immediately               = true
+  skip_final_snapshot             = true
+  auto_minor_version_upgrade      = true
+  enabled_cloudwatch_logs_exports = ["audit", "error", "slowquery"]
+  family                          = "mariadb10.6"
+  major_engine_version            = "10.6"
+  create_db_subnet_group          = true
+  db_subnet_group_name            = "demo_aws_account_name-mariadb-keycloak-rds-subnetgroup"
+  db_subnet_group_description     = "Managed by Terraform"
+  db_subnet_group_use_name_prefix = false
+  create_db_option_group          = true
+  option_group_name               = "demo_aws_account_name-mariadb-keycloak-rds-optiongroup"
+  option_group_use_name_prefix    = false
+  option_group_description        = "Managed by Terraform"
+  create_db_parameter_group       = true
+  parameter_group_name            = "demo_aws_account_name-mariadb-keycloak-rds-parametergroup"
+  parameter_group_use_name_prefix = false
+  parameter_group_description     = "demo_aws_account_name-mariadb-keycloak-rds-parametergroup"
+
+  tags = merge(
+    var.tags,
+    {
+      Team       = "Blog"
+      Project    = "Keycloak"
+      Backup     = "true"
+    },
+  )
+}
+
+## ALB Keycloack ##
+module "keycloak-internal-alb" {
+  source = "terraform-aws-modules/alb/aws"
+
+  name = "demo-keycloak-alb"
+
+  load_balancer_type = "application"
+  internal           = false
+  vpc_id             = module.global_vpc.vpc_id
+  subnets            = module.global_vpc.public_subnets
+  security_groups    = [module.keycloak-internal-alb-sg.security_group_id]
+
+  target_groups = [
+    {
+      name_prefix      = "keycl"
+      backend_protocol = "HTTP"
+      backend_port     = 8080
+      target_type      = "ip"
+      health_check = {
+        enabled             = true
+        interval            = 30
+        path                = "/health"
+        port                = "traffic-port"
+        healthy_threshold   = 5
+        unhealthy_threshold = 2
+        timeout             = 5
+        protocol            = "HTTP"
+        matcher             = "200-399"
+      }
+    }
+  ]
+
+  https_listeners = [
+    {
+      port                        = 443
+      protocol                    = "HTTPS"
+      listener_ssl_policy_default = "ELBSecurityPolicy-FS-1-2-Res-2020-10"
+      target_group_index          = 0
+    }
+  ]
+
+  http_tcp_listeners = [
+    {
+      port        = 80
+      protocol    = "HTTP"
+      target_group_index = 0
+    }
+  ]
+
+  tags = merge(
+    var.tags,
+    {
+      Env         = "Blog"
+      LegalEntity = "Demo"
+      Project     = "Keycloak"
+    }
+  )
+}
+
+## ECS Cluster##
+resource "aws_ecs_cluster" "demo-keycloak_ecs_cluster" {
+  capacity_providers = ["FARGATE"]
+
+  default_capacity_provider_strategy {
+    base              = "0"
+    capacity_provider = "FARGATE"
+    weight            = "1"
+  }
+
+  name = "demo_aws_account_name-keycloak-ecs"
+
+  setting {
+    name  = "containerInsights"
+    value = "disabled"
+  }
+
+  tags = merge(
+    var.tags,
+    {
+      Env         = "Demo"
+      Project     = "Keycloak"
+    }
+  )
+}
+
+## ECS Cluster Service
+resource "aws_ecs_service" "demo-keycloak_ecs_cluster_service" {
+  capacity_provider_strategy {
+    base              = "0"
+    capacity_provider = "FARGATE"
+    weight            = "1"
+  }
+
+  cluster = aws_ecs_cluster.demo-keycloak_ecs_cluster.name
+
+  deployment_circuit_breaker {
+    enable   = "false"
+    rollback = "false"
+  }
+
+  deployment_controller {
+    type = "ECS"
+  }
+
+  deployment_maximum_percent         = "200"
+  deployment_minimum_healthy_percent = "100"
+  desired_count                      = "1"
+  enable_ecs_managed_tags            = "true"
+  enable_execute_command             = "true"
+  health_check_grace_period_seconds  = "0"
+
+  load_balancer {
+    container_name   = "keycloak"
+    container_port   = "8080"
+    target_group_arn = module.keycloak-internal-alb.target_group_arns[0]
+  }
+
+  load_balancer {
+    container_name   = "keycloak"
+    container_port   = "8080"
+    target_group_arn = module.keycloak-secound-internal-alb.target_group_arns[0]
+  }
+
+  name = "demo_aws_account_name-keycloak-service"
+
+  network_configuration {
+    assign_public_ip = "false"
+    security_groups  = [aws_security_group.demo-ecs-keycloak-sg.id]
+    subnets          = module.global_vpc.private_subnets
+  }
+
+  platform_version    = "LATEST"
+  scheduling_strategy = "REPLICA"
+  task_definition     = aws_ecs_task_definition.demo-keycloak_ecs_task_definition.arn
+}
+
+resource "aws_ecs_task_definition" "demo-keycloak_ecs_task_definition" {
+  cpu                      = "1024"
+  execution_role_arn       = aws_iam_role.ecs_keycloak.arn
+  family                   = "keycloak"
+  memory                   = "2048"
+  container_definitions    = <<TASK_DEFINITION
+[
+  {
+    "cpu": 0,
+    "environment": [
+      {"name": "AWS_REGION", "value": "eu-central-1"},
+      {
+          "name": "KC_PROXY",
+          "value": "edge"
+      },
+      {
+          "name": "AWS_REGION",
+          "value": "eu-central-1"
+      },
+      {
+          "name": "KC_DB_URL_PORT",
+          "value": "3306"
+      },
+      {
+          "name": "KC_DB_USERNAME",
+          "value": "liquibase_keycloak"
+      },
+      {
+          "name": "KC_DB",
+          "value": "mariadb"
+      },
+      {
+          "name": "KC_DB_URL_HOST",
+          "value": "${module.rds_keycloak.db_instance_endpoint}"
+      },
+      {
+          "name": "KC_HOSTNAME_STRICT",
+          "value": "false"
+      },
+      {
+          "name": "KC_HOSTNAME",
+          "value": "auth.demo.cnl.io"
+      },
+      {
+          "name": "KC_HEALTH_ENABLED",
+          "value": "true"
+      },
+      {
+          "name": "PROXY_ADDRESS_FORWARDING",
+          "value": "true"
+      }
+    ],
+    "secrets": [
+      {"name": "KC_DB_PASSWORD", "valueFrom": "arn:aws:secretsmanager:eu-central-1:****:secret:keycloak/rds/credentials-xxyy:password::"}
+    ],
+    "essential": true,
+    "image": "****.dkr.ecr.eu-central-1.amazonaws.com/cpe/quay.io/keycloak/keycloak:20.0.5",
+    "logConfiguration": {
+                          "logDriver":"awslogs",
+                          "options": {
+                                        "awslogs-group":"demo-ecs-keycloak-log-group",
+                                        "awslogs-region": "eu-central-1",
+                                        "awslogs-stream-prefix":"ecs"
+                                      }
+                        },
+    "name": "keycloak",
+    "portMappings": [
+      {
+        "containerPort": 8080,
+        "hostPort": 8080,
+        "protocol": "tcp"
+      }
+    ]
+  }
+]
+TASK_DEFINITION
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  task_role_arn            = aws_iam_role.ecs_keycloak.arn
+
+  runtime_platform {
+    operating_system_family = "LINUX"
+  }
+  depends_on = [
+    aws_cloudwatch_log_group.ecs_keycloak_log_log_group
+  ]
+}
+
